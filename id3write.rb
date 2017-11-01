2eb5a962f8f26f0684f730540a922cd61a0e0a25
Robert WEILER <rbrt-weiler@mailbox.org>
2017-11-01 06:39:33 +0000

    Revised the script and converted it to English.

--- id3write.rb
+++ id3write.rb
@@ -1,42 +1,33 @@
 #!/usr/bin/ruby -w
 
-# Skript zum schnellen ID3-Taggen von MP3s.
+# Script for pushing ID3 information into MP3 files.
 
-# Die MP3s müssen nach dem Schema "Tracknr - Liedname.mp3" benannt sein.
-# ID3v1-Tags werden mit dem Programm mp3info geschrieben, ID3v2-Tags werden
-# mit dem Programm id3v2 geschrieben.
-# Künstler und Album können an der Shell mit den Parametern -a und -l
-# angegeben werden.
+# MP3 files must be named according to the "trackno - title.mp3" scheme.
+# ID3 information is written using the external programs mp3info and id3v2.
+# Artist and album can be passed using the parameters -a and -l.
 
-# (c) 2003 Robert Weiler <http://www.robwei.de/>
-# GPL'ed <http://www.gnu.org/licenses/gpl.html>
-# Dieses Skript kommt OHNE JEGLICHE GEWÄHRLEISTUNG.
-
-
+# (c) 2003 Robert Weiler
+# Released under the MIT license. <https://opensource.org/licenses/MIT>
 
 class MP3
 	@@count      = 0
 	@@count_good = 0
 	@@mp3list    = Hash.new
 
-
-
 	def MP3.counter
 		return [@@count, @@count_good, @@mp3list.length]
 	end
-
-
 
 	def MP3.retag
 		if (@@mp3list.length > 0)
 			@@mp3list = @@mp3list.sort
 			@@mp3list.each {
 				|key, value|
-				puts "Bearbeite #{value[3]}..."
-				puts "\tLoeschen des alten Tags..."
+				puts "Editing #{value[3]}..."
+				puts "\tDeleting the old tags..."
 				`mp3info -d "#{key}"`
 				`id3v2 -d "#{key}"`
-				puts "\tNeuen Tag schreiben..."
+				puts "\tWriting new tags..."
 				`mp3info -a "#{value[0]}" -l "#{value[1]}" -n "#{value[2]}" -t "#{value[3]}" -c "ID3v1 by id3write.rb" "#{key}"`
 				`id3v2 -a "#{value[0]}" -A "#{value[1]}" -T "#{value[2]}/#{@@mp3list.length}" -t "#{value[3]}" -c "ID3v2 by id3write.rb" "#{key}"`
 				@@count_good += 1
@@ -47,11 +38,7 @@ class MP3
 		end
 	end
 
-
-
 	attr_reader :filename, :artist, :album, :track, :title, :retagable
-
-
 
 	def initialize(filename, artist, album)
 		@@count += 1
@@ -74,24 +61,20 @@ class MP3
 	end
 end
 
-
-
-
-
 args = ''
 ARGV.each { |value|   args = args << value << " " }
 
 if (args =~ /-a "(.*?)"/  or  args =~ /-a (\w+)/)
 	artist = $1
 else
-	print 'Kuenstler?  '
+	print 'Artist?  '
 	artist = $stdin.gets
 end
 
 if (args =~ /-l "(.*?)"/  or  args =~ /-l (\w+)/)
 	album = $1
 else
-	print 'Album?      '
+	print 'Album?   '
 	album = $stdin.gets
 end
 
@@ -99,7 +82,8 @@ Dir['*'].each { |value|   MP3.new(value, artist.chomp, album.chomp) if (value =~
 
 if (MP3.counter[2] > 0)
 	MP3.retag
-	puts "\n#{MP3.counter[1]} von #{MP3.counter[0]} MP3s wurden umbenannt."
+	puts "\n#{MP3.counter[1]} out of #{MP3.counter[0]} MP3 files were tagged."
 else
-	puts 'Im aktuellen Verzeichnis wurden keine MP3s gefunden.'
+	puts 'No MP3 files found in current directory.'
 end
+
